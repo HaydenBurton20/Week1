d43a41ff227642c493c2b1d8a5ceaef6e365a3bb
HaydenBurton20 <101027937+HaydenBurton20@users.noreply.github.com>
2022-03-05 20:11:59 -0600

    Add files via upload

---
+++ Project2-HaydenBurton.py
@@ -0,0 +1,6 @@
+name = "Hayden Burton"
+address ="1801 E 4th"
+number = "9183444958"
+
+print(name, address, number)
+
---
+++ Project4.py
@@ -0,0 +1,4 @@
+width = int(input("Enter with Width: "))
+length = int(input("Enter with length: "))
+area = width * length
+print("The area is", area, "square units.")
